2439af98c089cafdf8298d872f94ffe8b5d2cf12
Andy Harris <aharris@guidewire.com>
2018-12-07 15:48:59 -0800

    Improved report formatting

--- src/test/java/io/cucumber/skeleton/RunCucumberTest.java
+++ src/test/java/io/cucumber/skeleton/RunCucumberTest.java
@@ -5,6 +5,10 @@ import cucumber.api.junit.Cucumber;
 import org.junit.runner.RunWith;
 
 @RunWith(Cucumber.class)
-@CucumberOptions(plugin = {"pretty"})
+@CucumberOptions(plugin = {
+        "pretty",
+        "html:target/cucumber-reports/cucumber-pretty",
+        "json:target/cucumber-reports/CucumberTestReport.json",
+        "rerun:target/cucumber-reports/rerun.txt"})
 public class RunCucumberTest {
 }
