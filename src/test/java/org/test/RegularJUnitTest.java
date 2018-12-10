4f51cdbe0a812d2650ac9710d8559c21e366049f
Andy Harris <aharris@guidewire.com>
2018-12-10 12:02:32 -0800

    Added a new test

---
+++ src/test/java/org/test/RegularJUnitTest.java
@@ -0,0 +1,32 @@
+package org.test;
+
+import io.cucumber.skeleton.MathsMachine;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class RegularJUnitTest {
+    @Test
+    public void testAdditionThatWorks() {
+        MathsMachine machine = new MathsMachine();
+
+        machine.setCurrentTotal(0);
+        machine.add(10);
+        machine.subtract(23);
+        machine.multiply(2);
+
+        assertEquals(-26, machine.getCurrentTotal());
+    }
+
+    @Test
+    public void testAdditionThatFails() {
+        MathsMachine machine = new MathsMachine();
+
+        machine.setCurrentTotal(0);
+        machine.add(10);
+        machine.subtract(23);
+        machine.multiply(2);
+
+        assertEquals("Testing the test with a deliberate failure", -27, machine.getCurrentTotal());
+    }
+}
